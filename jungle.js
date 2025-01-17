6ee98b17407f2d55dde6db2f03ad2e3ee4c12dc8
na1969na <nakmtn07@gmail.com>
2025-01-17 09:36:52 -0800

    done exercise

--- jungle.js
+++ jungle.js
@@ -1,17 +1,164 @@
+// Base Class: Animal
 class Animal {
+  static remainingAnimals = 0;
 
+  constructor(name, species, energy = 0) {
+    this._name = name;
+    this._species = species;
+    this._energy = energy;
+    Animal.remainingAnimals++;
+  }
+
+  // Getters and Setters
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get species() {
+    return this._species;
+  }
+
+  set species(value) {
+    this._species = value;
+  }
+
+  get energy() {
+    return this._energy;
+  }
+
+  set energy(value) {
+    if (value <= 0) {
+      this._energy = 0;
+      Animal.remainingAnimals--;
+    } else {
+      this._energy = value;
+    }
+  }
+
+  // Methods
+  attack(target) {
+    if (this._energy <= 0) {
+      console.log(`${this._name} has no energy to attack!`);
+      return;
+    }
+
+    this._energy -= 10;
+    target.energy -= 10;
+
+    if (target.energy === 0) {
+      console.log(`${this._name} wins! ${target.name} is out of energy!`);
+    }
+  }
+
+  eat() {
+    this._energy += 10;
+  }
 }
 
+// Subclass: Bird
 class Bird extends Animal {
-  
+  constructor(name, species, canFly = true) {
+    super(name, species, 100);
+    this._canFly = canFly;
+  }
+
+  get canFly() {
+    return this._canFly;
+  }
+
+  set canFly(value) {
+    this._canFly = value;
+  }
+
+  attack(target) {
+    if (this._energy <= 0) {
+      console.log(`${this._name} has no energy to attack!`);
+      return;
+    }
+
+    this._energy -= 20;
+    target.energy -= 20;
+
+    if (target.energy === 0) {
+      console.log(`${this._name} wins! ${target.name} is out of energy!`);
+    }
+  }
+
+  eat() {
+    this._energy += 10;
+  }
 }
 
+// Subclass: Mammal
 class Mammal extends Animal {
+  constructor(name, species, furColor) {
+    super(name, species, 200);
+    this._furColor = furColor;
+  }
 
+  get furColor() {
+    return this._furColor;
+  }
+
+  set furColor(value) {
+    this._furColor = value;
+  }
+
+  attack(target) {
+    if (this._energy <= 0) {
+      console.log(`${this._name} has no energy to attack!`);
+      return;
+    }
+
+    this._energy -= 50;
+    target.energy -= 50;
+
+    if (target.energy === 0) {
+      console.log(`${this._name} wins! ${target.name} is out of energy!`);
+    }
+  }
+
+  eat() {
+    this._energy += 20;
+  }
 }
 
+// Subclass: Reptile
 class Reptile extends Animal {
+  constructor(name, species, coldBlooded = true) {
+    super(name, species, 100);
+    this._coldBlooded = coldBlooded;
+  }
 
+  get coldBlooded() {
+    return this._coldBlooded;
+  }
+
+  set coldBlooded(value) {
+    this._coldBlooded = value;
+  }
+
+  attack(target) {
+    if (this._energy <= 0) {
+      console.log(`${this._name} has no energy to attack!`);
+      return;
+    }
+
+    this._energy -= 30;
+    target.energy -= 30;
+
+    if (target.energy === 0) {
+      console.log(`${this._name} wins! ${target.name} is out of energy!`);
+    }
+  }
+
+  eat() {
+    this._energy += 15;
+  }
 }
 
 
